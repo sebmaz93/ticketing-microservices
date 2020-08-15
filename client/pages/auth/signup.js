8fc0f2726b66a9f7d51384c0d97c74abc76e3833
SebMaz <seb.maz93@icloud.com>
2020-08-15 05:56:57 +0400

    add error handler next

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,17 +4,20 @@ import axios from 'axios';
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('/api/users/signup', {
+      await axios.post('/api/users/signup', {
         email,
         password,
       });
-      console.log(res.data);
-    } catch (e) {
-      console.error(e);
+      setErrors([]);
+      setEmail('');
+      setPassword('');
+    } catch (err) {
+      setErrors(err.response.data.errors);
     }
   };
 
@@ -39,6 +42,16 @@ const Signup = () => {
           value={password}
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err, idx) => (
+              <li key={idx}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
